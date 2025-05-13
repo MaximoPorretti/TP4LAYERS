473ee6cd717969831c77005356a8606905f7b531
MaximoPorretti <164427979+MaximoPorretti@users.noreply.github.com>
2025-05-13 02:35:06 -0300

    TP4 PUNTO 1

---
+++ oop2-layering/src/main/java/model/RegistroDeVentas.java
@@ -0,0 +1,14 @@
+package model;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface RegistroDeVentas {
+    void nuevaVenta(LocalDateTime fechaDeVenta,
+                    float montoTotal,
+                    float cantidadLitros,
+                    String tipo);
+
+    List<Venta> ventas();
+}
